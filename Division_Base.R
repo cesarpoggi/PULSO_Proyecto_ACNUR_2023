b48e81436227b34b9ecaacc89c3f5e0288a2abc6
César Poggi <cesar.poggi@pucp.edu.pe>
2023-03-12 23:19:00 -0500

    Nuevos documentos
    Corrección de reportes

--- Division_Base.R
+++ Division_Base.R
@@ -1,3 +1,5 @@
+#Para primera versión de la data
+
 library(dplyr)
 library(rio)
 colnames(dataF)[c(6)]<-c("asistencia")
@@ -21,3 +23,23 @@ colnames(dataVBG)[c(6)]<-c("group_Interview/type_assist_peru")
 export(dataCBI, "dataCBI.xlsx")
 export(dataHTL, "dataHTL.xlsx")
 export(dataVBG, "dataVBG.xlsx")
+
+
+#Para segunda versión de la data
+
+library(dplyr)
+library(rio)
+
+
+dataCBI_V2 <- dataF_V2 %>%
+  filter(`group_Interview/type_assist_peru` == "cbi")
+
+dataHTL_V2 <- dataF_V2 %>%
+  filter(`group_Interview/type_assist_peru` == "hotline")
+
+dataVBG_V2 <- dataF_V2 %>%
+  filter(`group_Interview/type_assist_peru` == "svbg")  
+
+export(dataCBI_V2, "dataCBI_V2.xlsx")
+export(dataHTL_V2, "dataHTL_V2.xlsx")
+export(dataVBG_V2, "dataVBG_V2.xlsx")
